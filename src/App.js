9812d35f3d78b207bb05b7770e6b54c7eb2bdb0e
Manolingam N <manolingam1996@gmail.com>
2020-07-14 19:00:52 +0530

    chat bot test integration

--- src/App.js
+++ src/App.js
@@ -5,21 +5,64 @@ import Form from "./pages/Form/Form";
 
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
+import RaidGuildIcon from "./assets/raidguild__crosses.png";
+
 import "./App.css";
 
-function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path='/'>
-                    <Home />
-                </Route>
-                <Route exact path='/consult'>
-                    <Form />
-                </Route>
-            </Switch>
-        </Router>
-    );
+const payload = [
+    {
+        type: "chips",
+        options: [
+            {
+                text: "Case Studies",
+                link: "https://cloud.google.com/dialogflow/case-studies",
+            },
+            {
+                text: "Docs",
+                link: "https://cloud.google.com/dialogflow/docs",
+            },
+            {
+                text: "Case Studies",
+                link: "https://cloud.google.com/dialogflow/case-studies",
+            },
+            {
+                text: "Docs",
+                link: "https://cloud.google.com/dialogflow/docs",
+            },
+        ],
+    },
+];
+
+class App extends React.Component {
+    componentDidMount() {
+        const dfMessenger = document.querySelector("df-messenger");
+        // dfMessenger.addEventListener("df-user-input-entered", function (event) {
+        //     if (event.detail.input.includes("help")) {
+        //         dfMessenger.renderCustomCard(payload);
+        //     }
+        // });
+    }
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                        <df-messenger
+                            chat-icon={RaidGuildIcon}
+                            intent='WELCOME'
+                            // chat-title='QUERY BOT'
+                            agent-id='ce67c101-d4c1-45da-bec8-0a89fbeab3d6'
+                            language-code='en'
+                        ></df-messenger>
+                    </Route>
+                    <Route exact path='/consult'>
+                        <Form />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;
